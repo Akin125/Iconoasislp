317520894090666d0044fac1d0fc2d61e1b91390
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-06-10 18:01:15 +0100

    back here

--- src/index.js
+++ src/index.js
@@ -2,12 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './App.css';
+import { HashRouter as Router, Route, Routes } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <Router>
+          <Routes>
+              <Route exact path="/" element={<App />} />
+          </Routes>
+      </Router>
   </React.StrictMode>
 );
 
