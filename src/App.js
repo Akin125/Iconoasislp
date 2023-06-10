cd2ad852c720d5813128a19e5d5ecbbc83f31d5f
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-06-10 16:26:33 +0100

    features done

--- src/App.js
+++ src/App.js
@@ -1,19 +1,24 @@
 import React from "react";
-import Heroimg from '../src/images/login.png'
+import Heroimg from '../src/images/login.png';
+import Searchimg from '../src/images/search.png';
+import Dowloadimg from '../src/images/dowload1.png';
+import Initstate from '../src/images/inistate.png'
 
 function App() {
   return (
     <div className="App">
       <div className="header">
         <div className="div-1">
-          <h1 className="brand">ICONOASIS</h1>
+            <a href="#" className="link"><h1 className="brand">ICONOASIS</h1></a>
         </div>
 
         <div className="div-2">
-          <span className='sp-1'>About</span>
-          <span className='sp-2'>Feature</span>
-          <span className='sp-3'>Try for free</span>
+            <a href="" className='link'><span className='sp-1' >About</span></a>
+            <a href="" className='link'><span className='sp-2' >Features</span></a>
+            <a href="http://ec2-3-21-134-116.us-east-2.compute.amazonaws.com" className='link'><span className='sp-2' >Try For Free</span></a>
+
         </div>
+
       </div>
 
       <div className="hero">
@@ -27,10 +32,89 @@ function App() {
             </div>
         </div>
           <div className="button--try">
-              <div>Try for free</div>
+              <a href="http://ec2-3-21-134-116.us-east-2.compute.amazonaws.com" className="link"><div>Try for free</div></a>
           </div>
       </div>
 
+        <div className="feature">
+            <div className="herotext spec">FEATURES</div>
+            <div className="searchImage">
+                <div className="ftcont">
+
+                    <div className="ftimg">
+                        <img src={Searchimg} className='scaleimg' alt=""/>
+                    </div>
+
+
+                    <div className="fttext">
+                        <div className="ftheadertext">
+                            Searchable Images
+                        </div>
+
+                        <div className="ftextcont">
+                            Quickly locate the ideal image
+                            or icon using our powerful,
+                            top-of-page search feature for
+                            instant results.
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+            <hr/>
+
+            <div className="searchImage dowloadimg">
+                <div className="ftcont">
+
+                    <div className="ftimg">
+                        <img src={Dowloadimg} className='scaleimg' alt=""/>
+                    </div>
+
+
+                    <div className="fttext">
+                        <div className="ftheadertext">
+                            Downloadable Images
+                        </div>
+
+                        <div className="ftextcont">
+                            Access our content effortlessly
+                            through the prominent download button
+                            for an instant and hassle-free experience.
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+            <hr/>
+            <div className="searchImage">
+                <div className="ftcont">
+
+                    <div className="ftimg">
+                        <img src={Initstate} className='scaleimg' alt=""/>
+                    </div>
+
+
+                    <div className="fttext">
+                        <div className="ftheadertext">
+                            Responsive Design
+                        </div>
+
+                        <div className="ftextcont">
+                            Effortlessly access our responsive website on any device for optimal viewing.
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
     </div>
   );
 }
