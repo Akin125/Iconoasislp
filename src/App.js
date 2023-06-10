24cf5dc0ece5afdb964c9efeda9dea64be9f305f
Odediran Philip Oluseyi <110476898+Akin125@users.noreply.github.com>
2023-06-10 15:06:47 +0100

    hero done

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Heroimg from '../src/images/login.png'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">
+        <div className="div-1">
+          <h1 className="brand">ICONOASIS</h1>
+        </div>
+
+        <div className="div-2">
+          <span className='sp-1'>About</span>
+          <span className='sp-2'>Feature</span>
+          <span className='sp-3'>Try for free</span>
+        </div>
+      </div>
+
+      <div className="hero">
+        <div className="herocontents">
+            <div className="herotext">
+                Your one-stop Photo destination
+            </div>
+
+            <div className="heroimg">
+                <img src={Heroimg} alt=""/>
+            </div>
+        </div>
+          <div className="button--try">
+              <div>Try for free</div>
+          </div>
+      </div>
+
     </div>
   );
 }
